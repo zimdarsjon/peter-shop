e01bb12e1bde89c7442e924f0a75f1078b7cac13
Jonathon <zimdarsj@kenyon.edu>
2025-03-16 14:18:50 -0500

    Menu Setup

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import DynamicDialog from 'primevue/dynamicdialog';
+import { useMenuStore } from './stores/menu';
+import { storeToRefs } from 'pinia';
+import Menubar from 'primevue/menubar';
+
+const menuStore = useMenuStore();
+
+const { menuItems} = storeToRefs(menuStore);
+
 </script>
 
 <template>
   <DynamicDialog />
+  <div class="card">
+    <Menubar :model="menuItems" />
+  </div>
   <main>
     <RouterView />
   </main>
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -21,5 +32,5 @@ import DynamicDialog from 'primevue/dynamicdialog';
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
+} */
 </style>
---
+++ src/stores/menu.ts
@@ -0,0 +1,58 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useMenuStore = defineStore('menu', () => {
+  
+    const menuItems = ref<any[]>([
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Components',
+                    icon: 'pi pi-bolt'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil'
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ]);
+
+
+    return {
+        menuItems
+    }
+        
+})
